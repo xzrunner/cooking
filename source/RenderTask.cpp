1087d87d8b4ef3d90e9a2ec95747fdf190bbcc78
zz <zhuguang@ejoy.com>
2017-08-26 09:46:12 +0800

    rm thread pool temporary

--- source/RenderTask.cpp
+++ source/RenderTask.cpp
@@ -9,7 +9,7 @@ uint32_t RenderTask::m_next_id = 0;
 
 RenderTask::RenderTask(void (*draw)(void* obj, void* context, DisplayList* dlist), 
 					   void* obj, void* context)
-	: mt::Task(TASK_TYPE, true)
+	: mt::Task(TASK_TYPE)
 	, m_id(m_next_id++)
 	, m_draw_cb(draw)
 	, m_obj(obj)
@@ -30,45 +30,45 @@ void RenderTask::Run()
 
 void ThreadPool::AddTask(RenderTask* task)
 {
-	mt::ThreadPool::Instance()->AddTask(task);
+//	mt::ThreadPool::Instance()->AddTask(task);
 }
 
 void ThreadPool::Flush()
 {
-	mt::ThreadPool::Instance()->Lock();
-
-	const int sz = mt::ThreadPool::THREAD_NUM;
-	mt::Task* tasks[sz];
-	mt::ThreadPool::Instance()->GetResult(RenderTask::TASK_TYPE, tasks);
-	
-	while (true)
-	{
-		RenderTask* task = NULL;
-		int task_idx = -1;
-
-		// choose
-		for (int i = 0; i < sz; ++i) 
-		{
-			mt::Task* t = tasks[i];
-			if (!t) {
-				continue;
-			}
-			RenderTask* rt = static_cast<RenderTask*>(t);
-			if (!task || rt->GetID() < task->GetID()) {
-				task = rt;
-				task_idx = i;
-			}
-		}
-
-		if (task) {
-			task->GetDisplayList().Replay();
-			tasks[task_idx] = tasks[task_idx]->GetNext();
-		} else {
-			break;
-		}
-	}
-
-	mt::ThreadPool::Instance()->Unlock();
+// 	mt::ThreadPool::Instance()->Lock();
+// 
+// 	const int sz = mt::ThreadPool::THREAD_NUM;
+// 	mt::Task* tasks[sz];
+// 	mt::ThreadPool::Instance()->GetResult(RenderTask::TASK_TYPE, tasks);
+// 	
+// 	while (true)
+// 	{
+// 		RenderTask* task = NULL;
+// 		int task_idx = -1;
+// 
+// 		// choose
+// 		for (int i = 0; i < sz; ++i) 
+// 		{
+// 			mt::Task* t = tasks[i];
+// 			if (!t) {
+// 				continue;
+// 			}
+// 			RenderTask* rt = static_cast<RenderTask*>(t);
+// 			if (!task || rt->GetID() < task->GetID()) {
+// 				task = rt;
+// 				task_idx = i;
+// 			}
+// 		}
+// 
+// 		if (task) {
+// 			task->GetDisplayList().Replay();
+// 			tasks[task_idx] = tasks[task_idx]->GetNext();
+// 		} else {
+// 			break;
+// 		}
+// 	}
+// 
+// 	mt::ThreadPool::Instance()->Unlock();
 }
 
 }
