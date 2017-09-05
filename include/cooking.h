aabb3d3dd978ff17ac2487c47ad43f8324c74ec6
zz <zhuguang@ejoy.com>
2017-09-05 08:43:37 +0800

    [CHANGED] rm add_task(), move flush()

--- include/cooking.h
+++ include/cooking.h
@@ -9,16 +9,20 @@ namespace cooking
 class DisplayList;
 class RenderTask;
 
+/************************************************************************/
+/* status                                                               */
+/************************************************************************/
+
 void change_shader(DisplayList* dl, int shader);
 
 void flush_shader(DisplayList* dl);
 
+/************************************************************************/
+/* draw                                                                 */
+/************************************************************************/
+
 void draw_quad(DisplayList* dl, uint32_t col_mul, uint32_t col_add, uint32_t col_rmap, 
 			   uint32_t col_gmap, uint32_t col_bmap, const float* vertices, const float* texcoords, int tex_id);
-
-void add_task(RenderTask* task);
-
-void flush();
 
 }
 
--- source/cooking.cpp
+++ source/cooking.cpp
@@ -8,6 +8,10 @@
 namespace cooking
 {
 
+/************************************************************************/
+/* status                                                               */
+/************************************************************************/
+
 void change_shader(DisplayList* dl, int shader)
 {
 	dl->AddOp(new (dl->GetAlloc()) ChangeShaderOp(shader));
@@ -18,20 +22,14 @@ void flush_shader(DisplayList* dl)
 	dl->AddOp(new (dl->GetAlloc()) FlushShaderOp());	
 }
 
+/************************************************************************/
+/* draw                                                                 */
+/************************************************************************/
+
 void draw_quad(DisplayList* dl, uint32_t col_mul, uint32_t col_add, uint32_t col_rmap, 
 			   uint32_t col_gmap, uint32_t col_bmap, const float* vertices, const float* texcoords, int tex_id)
 {
 	dl->AddOp(new (dl->GetAlloc()) DrawQuadOp(col_mul, col_add, col_rmap, col_gmap, col_bmap, vertices, texcoords, tex_id));	
-}
-
-void add_task(RenderTask* task)
-{
-	Callback::SubmitTask(task);
-}
-
-void flush()
-{
-	RenderTaskMgr::Instance()->Flush();
 }
 
 }
