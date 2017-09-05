51020d951738917ea182daabf19d552dba2ef24e
zz <zhuguang@ejoy.com>
2017-09-05 08:44:55 +0800

    [ADDED] cooking_flush();

---
+++ include/c_wrap_cooking.h
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef _cooking_wrap_c_h_
+#define _cooking_wrap_c_h_
+
+void cooking_flush();
+
+#endif // _cooking_wrap_c_h_
+
+#ifdef __cplusplus
+}
+#endif
---
+++ source/c_wrap_cooking.cpp
@@ -0,0 +1,14 @@
+//#include "c_wrap_cooking.h"
+
+#include "cooking/RenderTask.h"
+
+namespace cooking
+{
+
+extern "C"
+void cooking_flush()
+{
+	RenderTaskMgr::Instance()->Flush();
+}
+
+}
