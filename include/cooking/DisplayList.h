88d3e41344935a26b1716ad5e540d705f56fdd05
zz <zhuguang@ejoy.com>
2017-09-26 11:20:43 +0800

    [CHANGED] use block pool instead of vector

--- include/cooking/DisplayList.h
+++ include/cooking/DisplayList.h
@@ -3,6 +3,8 @@
 
 #include <memmgr/LinearAllocator.h>
 
+#include <assert.h>
+
 namespace cooking
 {
 
@@ -11,27 +13,88 @@ struct DisplayOp;
 class DisplayList
 {
 public:
-	DisplayList(mm::FreelistAllocator* freelist_alloc = nullptr, int capacity = -1);
+	DisplayList(mm::FreelistAllocator* freelist_alloc);
 	DisplayList(const DisplayList&) = delete;
 	DisplayList(const DisplayList&&) = delete;
 	DisplayList& operator = (const DisplayList&);
-	DisplayList& operator = (const DisplayList&&);
+	DisplayList& operator = (const DisplayList&&) = delete;
 	~DisplayList();
 
 	void Replay(int begin, int end);
 
-	void AddOp(DisplayOp* op) { m_ops.push_back(op); }
+	void AddOp(DisplayOp* op);
+
+	void DeepCopyFrom(const DisplayList& src, int pos, int count);
 	
 	mm::LinearAllocator& GetAlloc() { return m_alloc; }
 
-	bool Empty() const { return m_ops.empty(); }
+	bool Empty() const { return m_ops_sz == 0; }
 
-	size_t Size() const { return m_ops.size(); }
+	int Size() const { return m_ops_sz; }
+
+private:
+	static const int BLOCK_CAP = 128;
+
+	struct OpArrayBlock
+	{
+		OpArrayBlock()
+			: ops(nullptr)
+			, ops_sz(0)
+			, next(nullptr)
+		{}
+
+		bool IsFull() const { return ops_sz == BLOCK_CAP; }
+
+		void Add(DisplayOp* op) { ops[ops_sz++] = op; }
+
+		DisplayOp** ops;
+		int ops_sz;
+
+		OpArrayBlock* next;
+	};
+
+	struct OpArrayMemPool
+	{
+		OpArrayMemPool()
+			: freelist(nullptr) 
+		{}
+
+		OpArrayBlock* Alloc() 
+		{
+			OpArrayBlock* ret = nullptr;
+			if (freelist) {
+				ret = freelist;
+				freelist = freelist->next;
+				ret->ops_sz = 0;
+				ret->next = nullptr;
+			} else {
+				ret = (OpArrayBlock*)malloc(sizeof(OpArrayBlock) + sizeof(DisplayOp*) * BLOCK_CAP);
+				ret->ops = reinterpret_cast<DisplayOp**>(ret + 1);
+				ret->ops_sz = 0;
+				ret->next = nullptr;
+			}
+			return ret;
+		}
+		void Free(OpArrayBlock* b)
+		{
+			b->ops_sz = 0;
+			b->next = freelist;
+			freelist = b;
+		}
+
+		OpArrayBlock* freelist;
+	};
+
+private:
+	void ClearOps();
 
 private:
 	mm::LinearAllocator m_alloc;
 
-	std::vector<DisplayOp*> m_ops;
+	OpArrayBlock *m_ops_head, *m_ops_tail;
+	int m_ops_sz;
+
+	static OpArrayMemPool m_pool;
 
 }; // DisplayList
 
--- source/DisplayList.cpp
+++ source/DisplayList.cpp
@@ -1,21 +1,27 @@
 #include "cooking/DisplayList.h"
 #include "cooking/DisplayOp.h"
-#include "cooking/ReplayOpBuilder.h"
+#include "cooking/DisplayOpFunc.h"
+
+#include <algorithm>
+
+#include <assert.h>
 
 namespace cooking
 {
 
-DisplayList::DisplayList(mm::FreelistAllocator* freelist_alloc,
-	                     int capacity)
+DisplayList::OpArrayMemPool DisplayList::m_pool;
+
+DisplayList::DisplayList(mm::FreelistAllocator* freelist_alloc)
 	: m_alloc(freelist_alloc)
+	, m_ops_head(nullptr)
+	, m_ops_tail(nullptr)
+	, m_ops_sz(0)
 {
-	if (capacity > 0) {
-		m_ops.reserve(capacity);
-	}
 }
 
 DisplayList::~DisplayList()
 {
+	ClearOps();
 }
 
 DisplayList& DisplayList::operator = (const DisplayList& dlist)
@@ -24,31 +30,131 @@ DisplayList& DisplayList::operator = (const DisplayList& dlist)
 		return *this;
 	}
 
-	m_alloc.~LinearAllocator();
 	m_alloc = dlist.m_alloc;
 
-	m_ops = dlist.m_ops;
-
-	return *this;
-}
-
-DisplayList& DisplayList::operator = (const DisplayList&& dlist)
-{
-	if (this == &dlist) {
-		return *this;
-	}
-
-	m_alloc.~LinearAllocator();
-	m_alloc = std::move(dlist.m_alloc);
-
-	m_ops = std::move(dlist.m_ops);
+	ClearOps();
+	m_ops_head = dlist.m_ops_head;
+	m_ops_tail = dlist.m_ops_tail;
+	m_ops_sz = dlist.m_ops_sz;
+	const_cast<DisplayList&>(dlist).m_ops_head = nullptr;
+	const_cast<DisplayList&>(dlist).m_ops_tail = nullptr;
+	const_cast<DisplayList&>(dlist).m_ops_sz = 0;
 
 	return *this;
 }
 
 void DisplayList::Replay(int begin, int end)
 {
-	ReplayOpBuilder::Replay(m_ops, begin, end);
+	if (begin == end || m_ops_sz == 0) {
+		return;
+	}
+	if (begin < 0) {
+		begin = 0;
+	}
+	if (end < 0) {
+		end = m_ops_sz;
+	}
+	begin = std::min(begin, m_ops_sz);
+	end = std::min(end, m_ops_sz);
+
+	if (begin >= end || begin >= m_ops_sz) {
+		return;
+	}
+
+	OpArrayBlock* ptr_b = m_ops_head;
+	assert(ptr_b);
+	int ptr_b_start = 0;
+	while (ptr_b_start + BLOCK_CAP <= begin) {
+		ptr_b = ptr_b->next;
+		ptr_b_start += BLOCK_CAP;
+	}
+	assert(ptr_b_start + BLOCK_CAP > begin && begin >= ptr_b_start);
+
+	int ptr_op_idx = begin - ptr_b_start;
+	int left = end - begin;
+	while (left-- > 0)
+	{
+		DisplayOp* s_op = ptr_b->ops[ptr_op_idx];
+		DisplayOpFunc::Replay(s_op);
+		++ptr_op_idx;
+		if (ptr_op_idx == BLOCK_CAP) {
+			ptr_b = ptr_b->next;
+			ptr_op_idx = 0;
+		}
+	}
+}
+
+void DisplayList::AddOp(DisplayOp* op)
+{
+	if (!m_ops_head) 
+	{
+		assert(!m_ops_tail);
+		m_ops_head = m_ops_tail = m_pool.Alloc();
+		m_ops_head->Add(op);
+	} 
+	else 
+	{
+		if (m_ops_tail->IsFull()) {
+			OpArrayBlock* new_b = m_pool.Alloc();
+			m_ops_tail->next = new_b;
+			m_ops_tail = new_b;
+		}
+		m_ops_tail->Add(op);
+	}
+	++m_ops_sz;
+}
+
+void DisplayList::DeepCopyFrom(const DisplayList& src, int pos, int count)
+{
+	if (count == 0) {
+		return;
+	}
+	int sz = src.Size();
+	int begin = pos < 0 ? 0 : pos;
+	int end = count < 0 ? sz : begin + count;
+	if (begin == end) {
+		return;
+	}
+	assert(begin >= 0 && begin < sz && end >= 0 && end <= sz && begin < end);
+
+	OpArrayBlock* ptr_b = src.m_ops_head;
+	assert(ptr_b);
+	int ptr_b_start = 0;
+	while (ptr_b_start + BLOCK_CAP <= begin) {
+		ptr_b = ptr_b->next;
+		ptr_b_start += BLOCK_CAP;
+	}
+	assert(ptr_b_start + BLOCK_CAP > begin && begin >= ptr_b_start);
+
+	int ptr_op_idx = begin - ptr_b_start;
+	int left = end - begin;
+	while (left-- > 0)
+	{
+		DisplayOp* s_op = ptr_b->ops[ptr_op_idx];
+		size_t sz = DisplayOpFunc::Size(*s_op);
+		DisplayOp* d_op = reinterpret_cast<DisplayOp*>(m_alloc.alloc<char>(sz));
+		memcpy(d_op, s_op, sz);
+		AddOp(d_op);
+
+		++ptr_op_idx;
+		if (ptr_op_idx == BLOCK_CAP) {
+			ptr_b = ptr_b->next;
+			ptr_op_idx = 0;
+		}
+	}
+}
+
+void DisplayList::ClearOps()
+{
+	OpArrayBlock* b = m_ops_head;
+	while (b) {
+		OpArrayBlock* next = b->next;
+		m_pool.Free(b);
+		b = next;
+	}
+	m_ops_head = nullptr;
+	m_ops_tail = nullptr;
+	m_ops_sz = 0;
 }
 
 }
